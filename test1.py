54d3ae58065dc0ecdf9d7f188b596f010ee84bff
franleplant <franleplant@gmail.com>
2013-12-18 11:22:30 -0300

    refactor multi browser support

--- test1.py
+++ test1.py
@@ -3,20 +3,21 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
 
-
 import unittest
 
-class TestSequenceFunctions(unittest.TestCase):
+class Test_FireFox(unittest.TestCase):
 
-	def test_google_search_firefox(self):
-		# Create a new instance of the Firefox driver
-		driver = webdriver.Firefox()
+	driver = webdriver.Firefox();
+
+	def test_google_search(self):
+		# Create a new instance of the Firefox self.driver
+		#self.driver = webself.driver.Firefox()
 
 		# go to the google home page
-		driver.get("http://www.google.com")
+		self.driver.get("http://www.google.com")
 
 		# find the element that's name attribute is q (the google search box)
-		inputElement = driver.find_element_by_name("q")
+		inputElement = self.driver.find_element_by_name("q")
 
 		# type in the search
 		inputElement.send_keys("cheese!")
@@ -25,42 +26,26 @@ class TestSequenceFunctions(unittest.TestCase):
 		inputElement.submit()
 
 		# the page is ajaxy so the title is originally this:
-		print( driver.title )
+		print( self.driver.title )
 
 		try:
 		    # we have to wait for the page to refresh, the last thing that seems to be updated is the title
-		    WebDriverWait(driver, 10).until(EC.title_contains("cheese!"))
+		    WebDriverWait(self.driver, 10).until(EC.title_contains("cheese!"))
 
 		    # You should see "cheese! - Google Search"
-		    print( driver.title )
-		    self.assertTrue(  driver.title.find(  "cheese!" ) != -1 )
+		    print( self.driver.title )
+		    self.assertTrue(  self.driver.title.find(  "cheese!" ) != -1 )
 
 		finally:
-		    driver.quit()
+		    self.driver.quit()
 
-
-	def test_google_search_chrome(self):
-		driver = webdriver.Chrome()
-
-		driver.get("http://www.google.com")
-
-		inputElement = driver.find_element_by_name("q")
-		inputElement.send_keys("cheese!")
-
-		inputElement.submit()
-		print( driver.title )
-
-		try:
-		    WebDriverWait(driver, 10).until(EC.title_contains("cheese!"))
-
-		    print( driver.title )
-		    self.assertTrue(  driver.title.find(  "cheese!" ) != -1 )
-
-		finally:
-		    driver.quit()
 
         
-        
+class Test_Chrome(Test_FireFox):
+
+	driver = webdriver.Chrome()
+
+
 
 
 
